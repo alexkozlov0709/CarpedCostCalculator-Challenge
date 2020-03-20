1f227246306379f115c0075242fc628f958624ea
alexkozlov0709 <59420903+alexkozlov0709@users.noreply.github.com>
2020-03-20 21:59:01 +0200

    Create Floor.java

---
+++ Floor.java
@@ -0,0 +1,18 @@
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width;
+        this.length = length;
+        if (length < 0) {
+            this.length = 0;
+        } else if (width < 0) {
+           this.width = 0;
+        }
+    }
+
+    public double getArea() {
+        return this.width*length;
+    }
+}
